0490214ebff2aaf3b9f428d42343d59c915088b2
andrii vakariuk <andrii.vakariuk@sapient.pro>
2022-12-23 17:22:14 +0200

    added vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs';
+// import {createApp} from 'vue'
+import { createApp } from 'vue/dist/vue.esm-bundler';
+import PostIndex from './components/Posts/Index.vue'
 
-window.Alpine = Alpine;
-
-Alpine.start();
+const app = createApp({})
+app.component('post-index', PostIndex)
+app.mount("#app")
