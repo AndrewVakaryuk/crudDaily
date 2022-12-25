2505fc12f77b3b401aaad43c0e1bddfc93499d38
andrii vakariuk <andrii.vakariuk@sapient.pro>
2022-12-26 01:23:09 +0200

    filter data

--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
         <div class="min-w-full align-middle">
+            <div class="mb-4">
+                <select v-model="selectedCategory" class="block mt-1 w-full sm:w-1/4 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                    <option value="" selected>-- Filter by category --</option>
+                    <option v-for="category in categories" :value="category.id">
+                        {{ category.name }}
+                    </option>
+                </select>
+            </div>
             <table class="min-w-full divide-y divide-gray-200 border">
                 <thead>
                 <tr>
@@ -41,19 +49,28 @@
                 </tr>
                 </tbody>
             </table>
-            <Pagination :data="posts" @pagination-change-page="getPosts" />
+            <Pagination :data="posts" @pagination-change-page="page => getPosts(page, selectedCategory)" />
         </div>
     </div>
 </template>
 
 <script>
-import { onMounted } from "vue";
+import { ref, onMounted, watch } from "vue";
 import usePosts from "../../composables/posts";
+import useCategories from "../../composables/categories";
 export default {
     setup() {
+        const selectedCategory = ref('')
         const { posts, getPosts } = usePosts()
-        onMounted(getPosts)
-        return { posts, getPosts }
+        const { categories, getCategories } = useCategories()
+        onMounted(() => {
+            getPosts()
+            getCategories()
+        })
+        watch(selectedCategory, (current, previous) => {
+            getPosts(1, current)
+        })
+        return { posts, getPosts, categories, selectedCategory }
     }
 }
 </script>
