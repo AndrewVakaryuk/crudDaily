ac775daf692527db028f2f8ffefb7447f49fc001
andrii vakariuk <andrii.vakariuk@sapient.pro>
2022-12-26 00:15:28 +0200

    pagination

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,10 @@ import './bootstrap';
 
 // import {createApp} from 'vue'
 import { createApp } from 'vue/dist/vue.esm-bundler';
-import PostIndex from './components/Posts/Index.vue'
+import PostIndex from './components/Posts/Index.vue';
+import { TailwindPagination } from 'laravel-vue-pagination';
 
 const app = createApp({})
 app.component('post-index', PostIndex)
+app.component('Pagination', TailwindPagination)
 app.mount("#app")
