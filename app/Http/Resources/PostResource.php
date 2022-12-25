966875b6557cf12bcfd7c25edd374e4fec45315a
andrii vakariuk <andrii.vakariuk@sapient.pro>
2022-12-25 23:47:46 +0200

    API Resource

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use JsonSerializable;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array|Arrayable|JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => substr($this->content, 0, 50) . '...',
+            'created_at' => $this->created_at->toDateString()
+        ];
+    }
+}
