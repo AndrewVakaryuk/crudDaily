42c4a99a3738b6ad30e85eca9d8808b5f5e199fe
andrii vakariuk <andrii.vakariuk@sapient.pro>
2022-12-27 17:05:30 +0200

    layouts

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,14 +2,13 @@ import './bootstrap';
 
 // import {createApp} from 'vue'
 import { createApp } from 'vue/dist/vue.esm-bundler'
-import App from './layouts/App.vue'
 import { TailwindPagination } from 'laravel-vue-pagination'
 import router from './routes/index'
 import VueSweetalert2 from "vue-sweetalert2";
 import 'sweetalert2/dist/sweetalert2.min.css';
 
 
-const app = createApp(App)
+const app = createApp({})
 app.use(router)
 app.use(VueSweetalert2)
 app.component('Pagination', TailwindPagination)
