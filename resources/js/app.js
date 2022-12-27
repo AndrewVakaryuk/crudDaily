9205164ed31118e5286eb5bb08d6b715e3443f36
andrii vakariuk <andrii.vakariuk@sapient.pro>
2022-12-27 12:43:11 +0200

    pass parameter through router to vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,23 +6,6 @@ import App from './layouts/App.vue'
 import { TailwindPagination } from 'laravel-vue-pagination'
 import router from './routes/index'
 
-// const routes = [
-//     {
-//         path: '/',
-//         name: 'posts.index',
-//         component: PostsIndex
-//     },
-//     {
-//         path: '/posts/create',
-//         name: 'posts.create',
-//         component: PostsCreate
-//     },
-// ]
-//
-// const router = createRouter({
-//     history: createWebHistory(),
-//     routes
-// })
 
 const app = createApp(App)
 app.use(router)
