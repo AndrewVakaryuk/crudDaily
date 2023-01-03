102e7db079944c8875e86747eb08a0abe0546690
andrii vakariuk <andrii.vakariuk@sapient.pro>
2023-01-03 11:19:48 +0200

    added logout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,20 @@
 import './bootstrap';
 
 // import {createApp} from 'vue'
-import { createApp } from 'vue/dist/vue.esm-bundler'
+import { createApp, onMounted } from 'vue/dist/vue.esm-bundler'
 import { TailwindPagination } from 'laravel-vue-pagination'
 import router from './routes/index'
 import VueSweetalert2 from "vue-sweetalert2";
 import 'sweetalert2/dist/sweetalert2.min.css';
+import useAuth from "./composables/auth";
 
 
-const app = createApp({})
+const app = createApp({
+    setup() {
+        const { getUser } = useAuth()
+        onMounted(getUser)
+    }
+})
 app.use(router)
 app.use(VueSweetalert2)
 app.component('Pagination', TailwindPagination)
